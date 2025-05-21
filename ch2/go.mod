b83f7195cac6f58f12a0b79846726b901803469d
kuluruvineeth <kuluruvineeth8623@gmail.com>
2025-05-21 13:08:34 +0530

    Add chapter 2 files: main.go and go.mod
    - Introduced main.go with constants and commented examples for complex numbers, type conversions, and variable naming conventions.
    - Created go.mod for module management in chapter 2.
    - Added a binary file for chapter 2.

---
+++ ch2/go.mod
@@ -0,0 +1,3 @@
+module github.com/go-systems-lab/learning-go/ch2
+
+go 1.24.2
---
+++ ch2/main.go
@@ -0,0 +1,63 @@
+package main
+
+const x int64 = 10
+
+const (
+	idKey   = "id"
+	nameKey = "name"
+)
+
+const z = 20 * 10
+
+func main() {
+	// Complex Numbers
+	// x := complex(2.5, 3.1)
+	// y := complex(10.2, 2)
+	// fmt.Println(x + y)
+	// fmt.Println(x - y)
+	// fmt.Println(x * y)
+	// fmt.Println(x / y)
+	// fmt.Println(real(x))
+	// fmt.Println(imag(x))
+	// fmt.Println(cmplx.Abs(x))
+
+	// Type conversions
+	// var x int = 10
+	// var y float64 = 30.2
+	// var sum1 float64 = float64(x) + y
+	// var sum2 int = x + int(y)
+	// fmt.Println(sum1, sum2)
+
+	// Integer Type conversions
+	// var x int = 10
+	// var b byte = 100
+	// var sum3 int = x + int(b)
+	// var sum4 byte = byte(x) + b
+	// fmt.Println(sum3, sum4)
+
+	// Const
+	// const y = "hello"
+	// fmt.Println(x)
+	// fmt.Println(y)
+
+	// x = x + 1 //this will not compile
+	// y = "bye" //this will not compile
+	// fmt.Println(x)
+	// fmt.Println(y)
+
+	// Never use variable names like this
+	// _0 := 0_0
+	// _1 := 20
+	// a := "hello"
+	// __ := "double underscore"
+	// fmt.Println(_0)
+	// fmt.Println(_1)
+	// fmt.Println(a)
+	// fmt.Println(__)
+
+	// a := "hello"
+	// a_ := "goodbye"
+	// fmt.Println(a)
+	// fmt.Println(a_)
+
+}
